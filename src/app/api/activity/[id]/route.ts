5731ee8858d18bc627a94836bee03202f555adc9
zyaddj <zyadmancov@gmail.com>
2025-02-10 14:11:05 -0500

    fix

--- src/app/api/activity/[id]/route.ts
+++ src/app/api/activity/[id]/route.ts
@@ -2,12 +2,12 @@ import { NextResponse } from "next/server";
 
 const BASE_API = "https://api.yelp.com/v3/events";
 
-export async function GET(req: Request, { params }: { params: { id: string } }) {
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
   try {
-    // const { searchParams } = new URL(req.url);
-    const dynamicParams = await params;
-
-    const response = await fetch(`${BASE_API}/${dynamicParams.id}`, {
+    const response = await fetch(`${BASE_API}/${params.id}`, {
       headers: {
         accept: "application/json",
         authorization: `Bearer ${process.env.YELP_ACCESS_TOKEN}`,
